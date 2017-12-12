22fbb67e22424a1d30827d8ba6dc1911ec661cdc
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-12 16:34:20 +0800

    update

--- Number.js
+++ Number.js
@@ -2,5 +2,27 @@
 // 备注：
 
 // 属性
+Number.EPSILON
+Number.MAX_SAFE_INTEGER
+Number.MAX_VALUE
+Number.MIN_SAFE_INTEGER
+Number.MIN_VALUE
+Number.NEGATIVE_INFINITY
+Number.NaN
+Number.POSITIVE_INFINITY
+Number.prototype
 
 // 方法
+Number.isFinite()
+Number.isInteger()
+Number.isNaN()
+Number.isSafeInteger()
+Number.parseFloat()
+Number.parseInt()
+Number.prototype.toExponential()
+Number.prototype.toFixed()
+Number.prototype.toLocaleString()
+Number.prototype.toPrecision()
+Number.prototype.toSource()
+Number.prototype.toString()
+Number.prototype.valueOf()
