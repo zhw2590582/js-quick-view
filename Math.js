be546a9e1e02ff46390d575c43c56db175ad27b5
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-14 10:40:21 +0800

    update

--- Math.js
+++ Math.js
@@ -1,49 +1,49 @@
 // 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math
-// 备注：无
+// 备注：需要一定数学知识
 
 // 属性
-Math.E
-Math.LN10
-Math.LN2
-Math.LOG10E
-Math.LOG2E
-Math.PI
-Math.SQRT1_2
-Math.SQRT2
+Math.E // 表示自然对数的底数（或称为基数）
+Math.LN10 // 属性表示 10 的自然对数，约为 2.302
+Math.LN2 // 表示 2 的自然对数，约为 0.693
+Math.LOG10E // 表示以 10 为底数，e 的对数，约为 0.434
+Math.LOG2E // 表示以 2 为底数，e 的对数，约为 1.442
+Math.PI // 一个圆的周长与直径的比例，约为 3.14159
+Math.SQRT1_2 // 表示 1/2 的平方根，约为 0.707
+Math.SQRT2 // 属性表示 2 的平方根，约为 1.414
 
 // 方法
-Math.abs()
-Math.acos()
-Math.acosh()
-Math.asin()
-Math.asinh()
-Math.atan()
-Math.atan2()
-Math.atanh()
-Math.cbrt()
-Math.ceil()
-Math.clz32()
-Math.cos()
-Math.cosh()
-Math.exp()
-Math.expm1()
-Math.floor()
-Math.fround()
-Math.hypot()
-Math.imul()
-Math.log()
-Math.log10()
-Math.log1p()
-Math.log2()
-Math.max()
-Math.min()
-Math.pow()
-Math.random()
-Math.round()
-Math.sign()
-Math.sin()
-Math.sinh()
-Math.sqrt()
-Math.tan()
-Math.tanh()
-Math.trunc()
+Math.abs() // 返回指定数字的绝对值
+Math.acos() // 返回一个数的反余弦值（单位为弧度）
+Math.acosh() // 返回一个数字的反双曲余弦值
+Math.asin() // 返回一个数值的反正弦（单位为弧度）
+Math.asinh() // 返回给定数字的反双曲正弦值
+Math.atan() // 返回一个数值的反正切（以弧度为单位）
+Math.atan2() // 返回其参数比值的反正切值
+Math.atanh() // 返回一个数值反双曲正切值
+Math.cbrt() // 返回任意数字的立方根
+Math.ceil() // 返回大于或等于一个给定数字的最小整数, 向上取整
+Math.clz32() // 返回一个数字在转换成 32 无符号整形数字的二进制形式后, 开头的 0 的个数
+Math.cos() // 返回一个数值的余弦值
+Math.cosh() // 返回数值的双曲余弦函数
+Math.exp() // 函数返回 ex
+Math.expm1() // 返回 Ex - 1
+Math.floor() // 返回小于或等于一个给定数字的最大整数, 向下取整
+Math.fround() // 将任意的数字转换为离它最近的单精度浮点数形式的数字
+Math.hypot() // 回它的所有参数的平方和的平方根
+Math.imul() // 返回两个参数的类C的32位整数乘法运算的运算结果
+Math.log() // 返回一个数的自然对数
+Math.log10() // 返回一个数字以 10 为底的对数
+Math.log1p() // 返回一个数字加1后的自然对数 (底为 E), 既log(x+1)
+Math.log2() // 回一个数字以 2 为底的对数
+Math.max() // 返回一组数中的最大值
+Math.min() // 返回零个或更多个数值的最小值
+Math.pow() // 函数返回基数（base）的指数（exponent）次幂
+Math.random() // 返回一个浮点, 伪随机数在范围[0，1)
+Math.round() // 返回一个数字四舍五入后最接近的整数值
+Math.sign() // 返回一个数字的符号, 指示数字是正数，负数还是零
+Math.sin() // 返回一个数值的正弦值
+Math.sinh() // 返回一个数字(单位为角度)的双曲正弦值
+Math.sqrt() // 返回一个数的平方根
+Math.tan() // 返回一个数值的正切值
+Math.tanh() // 回一个数的双曲正切函数值
+Math.trunc() // 将数字的小数部分去掉，只保留整数部分
