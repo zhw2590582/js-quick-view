cb86c25d51a5b3a733f947f4bf6ab1c9ac4945cf
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-14 12:19:22 +0800

    update

---
+++ JSON.js
@@ -0,0 +1,6 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+// 备注：无
+
+// 方法
+JSON.parse() // 用来解析JSON字符串
+JSON.stringify() // 将一个JavaScript值(对象或者数组)转换为一个JSON字符串
---
+++ NaN.js
@@ -0,0 +1,2 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/NaN
+// 备注：全局属性 NaN 表示 Not-A-Number 的值
