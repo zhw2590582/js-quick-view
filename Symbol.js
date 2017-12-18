d98f965584e45f6a9b0ce2b2b65254730d8a3c94
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-18 17:33:19 +0800

    update

---
+++ Symbol.js
@@ -0,0 +1,23 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+// 备注：无
+
+// 属性
+Symbol.hasInstance
+Symbol.isConcatSpreadable
+Symbol.iterator
+Symbol.match
+Symbol.prototype
+Symbol.replace
+Symbol.search
+Symbol.species
+Symbol.split
+Symbol.toPrimitive
+Symbol.toStringTag
+Symbol.unscopables
+
+// 方法
+Symbol.for()
+Symbol.keyFor()
+Symbol.prototype.toSource()
+Symbol.prototype.toString()
+Symbol.prototype.valueOf()
