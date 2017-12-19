8f9072d8f6e704055e41f83f5620ce23a7d7170d
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-19 11:36:37 +0800

    update

---
+++ ArrayBuffer.js
@@ -0,0 +1,11 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
+// 备注：省略了Symbol部分
+
+// 属性
+ArrayBuffer.prototype
+ArrayBuffer.prototype.byteLength
+
+// 方法
+ArrayBuffer.isView()
+ArrayBuffer.prototype.slice()
+ArrayBuffer.transfer()
---
+++ Infinity.js
@@ -0,0 +1,2 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Infinity
+// 备注：全局属性 Infinity 是一个数值，表示无穷大
