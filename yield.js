806ee1bd4570e1ef61db4a1384e4f5776179932e
Harvey Zack <laozhaochaguan@hotmail.com>
2017-12-19 17:28:20 +0800

    update

--- yield.js
+++ yield.js
@@ -1,3 +1,17 @@
 // 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/yield
 // 备注：无
 
+// yield 关键字用来暂停和恢复一个生成器函数
+
+function* countAppleSales() {
+    var saleList = [3, 7, 5];
+    for (var i = 0; i < saleList.length; i++) {
+        yield saleList[i];
+    }
+}
+
+var appleStore = countAppleSales(); // Generator { }
+console.log(appleStore.next()); // { value: 3, done: false }
+console.log(appleStore.next()); // { value: 7, done: false }
+console.log(appleStore.next()); // { value: 5, done: false }
+console.log(appleStore.next()); // { value: undefined, done: true }
---
+++ yield_star.js
@@ -0,0 +1,25 @@
+// 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/yield*
+// 备注：无
+
+// yield* 表达式用于委托给另一个generator 或可迭代对象
+
+function* g1() {
+  yield 2;
+  yield 3;
+  yield 4;
+}
+
+function* g2() {
+  yield 1;
+  yield* g1();
+  yield 5;
+}
+
+var iterator = g2();
+
+console.log(iterator.next()); // { value: 1, done: false }
+console.log(iterator.next()); // { value: 2, done: false }
+console.log(iterator.next()); // { value: 3, done: false }
+console.log(iterator.next()); // { value: 4, done: false }
+console.log(iterator.next()); // { value: 5, done: false }
+console.log(iterator.next()); // { value: undefined, done: true }
